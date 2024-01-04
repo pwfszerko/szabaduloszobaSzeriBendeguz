8a51c140b62784accc4b44c1dec817c40d43aebd
pwfszerko <112931602+pwfszerko@users.noreply.github.com>
2024-01-05 00:32:06 +0100

    Update app-routing.module.ts

--- src/app/foglalasok/app-routing.module.ts
+++ src/app/foglalasok/app-routing.module.ts
@@ -2,7 +2,6 @@ import { Routes } from "@angular/router";
 import { FoglalasokComponent } from "./foglalasok.component";
 
 const routes: Routes = [
-    // További útvonalak
     { path: 'foglalasok', component: FoglalasokComponent }
   ];
   
