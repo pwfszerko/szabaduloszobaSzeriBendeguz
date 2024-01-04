5656463cfc2799451125b911e6a99a2cc12bbf87
pwfszerko <112931602+pwfszerko@users.noreply.github.com>
2024-01-05 00:30:03 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'szabaduloszoba-pelda-bela';
+  title = 'szabaduloszoba-szeri-bendeguz';
 }
